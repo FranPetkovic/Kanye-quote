f5d2d574c306b8d4c93e98e232c86bdf19258c18
Fran Petković <fran.petkovic1@gmail.com>
2022-07-21 16:56:44 +0200

    Updates

---
+++ precache-manifest.bce155d89f4eefe4547878ed41f24c73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "66985b36c5610ea1f568",
+    "url": "/Kanye-quote/static/css/main.4c1f2c09.chunk.css"
+  },
+  {
+    "revision": "66985b36c5610ea1f568",
+    "url": "/Kanye-quote/static/js/main.66985b36.chunk.js"
+  },
+  {
+    "revision": "10e59a8baf6e9a2d1eea",
+    "url": "/Kanye-quote/static/js/1.10e59a8b.chunk.js"
+  },
+  {
+    "revision": "949d16a38e6d9c18c24c",
+    "url": "/Kanye-quote/static/js/2.949d16a3.chunk.js"
+  },
+  {
+    "revision": "b00a0770a614b51e0aae",
+    "url": "/Kanye-quote/static/js/runtime~main.b00a0770.js"
+  },
+  {
+    "revision": "835a032cb8d74c4737d179027e1c183f",
+    "url": "/Kanye-quote/index.html"
+  }
+];
